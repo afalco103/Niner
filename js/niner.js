3b7de0dfb8da5eb34a5861ca08a00d03c81a6938
afalco103 <afalco103@gmail.com>
2016-01-24 14:23:49 -0500

    had old js by mistake

--- js/niner.js
+++ js/niner.js
@@ -5,12 +5,21 @@ $(document).ready(function () {
     highscore = 0;
     gamesPlayed = -1;
     average = 0;
-    max = size*size;
+    max = size * size;
 
     initGrid();
     $("div.controls").hide();
     $("#reset").click(function () {
-        aggScores();
+        //aggregate scores
+        if (score > highscore) {
+            highscore = score;
+            $("#highscore").text("High Score: " + highscore);
+        }
+        gamesPlayed++;
+        if (gamesPlayed != 0) {
+            average = (average * (gamesPlayed - 1) + score) / gamesPlayed;
+            $("#average").text("Average Score: " + Math.round(average));
+        }
         initGrid();
     });
 
@@ -22,16 +31,18 @@ $(document).ready(function () {
 
 function niner(button) {
     if (!setBox(button)) return;
-    
+
     if (box2 == null) return;
-    addEmUp();
-    setBoxValues();
-    setScore();
-}
 
+    //add numbers
+    var s1 = num1;
+    var s2 = num2;
+    sum = s1 + s2;
+    dif = Math.abs(s1 - s2);
+    num1 = null;
+    num2 = null;
 
-
-function setBoxValues() {
+    //set box values
     var source = $("#row" + box1.x + " #col" + box1.y);
     var target = $("#row" + box2.x + " #col" + box2.y);
     source.text(dif);
@@ -40,7 +51,11 @@ function setBoxValues() {
     setColor(source);
     setColor(target);
     deselect();
+
+    setScore();
 }
+
+
 
 function deselect() {
     box1 = null;
@@ -88,46 +103,37 @@ function initGrid() {
     var toAppend;
     $("#grid").html("");
 
+    //set table elements 
     for (var x = 0; x < size; x++) {
         toAppend += "<tr id=\"row" + x + "\">";
         for (var y = 0; y < size; y++) {
-            toAppend += "<td id=\"col" + y + "\">"+Math.floor(Math.random() * max)+"</td>";
+            toAppend += "<td id=\"col" + y + "\">" + Math.floor(Math.random() * max) + "</td>";
         }
         toAppend += "</tr>";
     }
-
     $("#grid").append(toAppend);
-
     $("#grid td").click(function () {
         niner(this);
     });
+    //set values so numbers arent too high or too low
     levelGrid();
+    //set some text
     $("#score").text("Score: " + score);
     $("#target").text("Get all of the boxes to be as close to " + max + " as possible. The game ends when you choose to end it. You don't really win until you get a perfect score.");
 }
-function aggScores() {
-    if (score > highscore) {
-        highscore = score;
-        $("#highscore").text("High Score: " + highscore);
-    }
-    gamesPlayed++;
-    if (gamesPlayed != 0) {
-        average = (average * (gamesPlayed - 1) + score) / gamesPlayed;
-        $("#average").text("Average Score: " + Math.round(average));
-    }
-}
+
 function levelGrid() {
     var total = 0;
     for (var x = 0; x < size; x++) {
         for (var y = 0; y < size; y++) {
-            total+=Number($("#row"+x+" #col"+y).text());
+            total += Number($("#row" + x + " #col" + y).text());
         }
     }
 
     var inc = 1;
     var avg = Math.round(((max - 1) / 2) * size * size);
     if (avg % 2 == 1) avg--;
-    avg = Math.round(avg*3/4);
+    avg = Math.round(avg * 3 / 4);
     score = avg;
 
     if (total == 2) return;
@@ -146,10 +152,13 @@ function levelGrid() {
     }
 }
 function setBox(button) {
+    //get box location
     var boxX = $(button).parent().attr("id").substr(3, 1);
     var boxY = $(button).attr("id").substr(3, 1);
+
     $(button).addClass("selected");
     num = Number($(button).text());
+
     if (box1 == null) {
         box1 = {
             x: boxX,
@@ -172,15 +181,6 @@ function setBox(button) {
 
 }
 
-function addEmUp() {
-    var s1 = num1;
-    var s2 = num2;
-    sum = s1 + s2;
-    dif = Math.abs(s1 - s2);
-    num1 = null;
-    num2 = null;
-}
-
 function isValid(x, y) {
     var distX = Math.abs(box2.x - box1.x);
     var distY = Math.abs(box2.y - box1.y);
@@ -191,7 +191,7 @@ function isValid(x, y) {
 
 function incSize(x) {
     size += x;
-    if (size < 2 || size>10) {
+    if (size < 2 || size > 10) {
         size -= x;
         return;
     }
@@ -204,8 +204,8 @@ function incSize(x) {
 
 function incMax(x) {
     max += x;
-    if (max < 2 || max>256) {
-        max -=x;
+    if (max < 2 || max > 256) {
+        max -= x;
         return;
     }
     average = 0;
